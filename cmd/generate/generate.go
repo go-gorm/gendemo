fb74e25dedef94e2515c6146f358ebf90f32cd64
qqxhb <1252905006@qq.com>
2023-10-28 17:28:20 +0800

    feat: rm db

--- cmd/generate/generate.go
+++ cmd/generate/generate.go
@@ -3,8 +3,6 @@ package main
 import (
 	"github.com/go-gorm/gendemo/biz/dal/model"
 	"gorm.io/gen"
-	"gorm.io/gorm"
-	"gorm.io/rawsql"
 )
 
 // GEN Guideline: https://gorm.io/gen/index.html
@@ -14,13 +12,6 @@ func main() {
 	//init db
 	//mysql.Init()
 	//db := mysql.DB(context.Background())
-	db, _ := gorm.Open(rawsql.New(rawsql.Config{
-		//SQL:      rawsql,                      //指定建表sql字符串也行
-		FilePath: []string{
-			//"./sql/user.sql", // 可以是sql文件,文件里可以是单个建表语句，也可以是多个
-			"../sql/", // 指定一个sql文件路径也可以
-		},
-	}))
 	// specify the output directory (default: "./query")
 	// ### if you want to query without context constrain, set mode gen.WithoutContext ###
 	g := gen.NewGenerator(gen.Config{
